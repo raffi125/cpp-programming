817320e435375260dd0f9bbcbb2dc2f923f4ac5b
erickpaulus <erick_paulus@yahoo.com>
2024-09-23 15:47:39 +0200

    update code

--- kode/sesi-05/sesi05_nested_for_04.cpp
+++ kode/sesi-05/sesi05_nested_for_04.cpp
@@ -15,7 +15,7 @@ int main() {
     int baris =0;
     int kolom = 0;
     int nilai = 0;
-
+    // fix this logic to print diamond
     for (size_t baris  = 0; baris < 5 ; baris++)
     {
         for (size_t kolom = 0; kolom <= baris ; kolom++)
