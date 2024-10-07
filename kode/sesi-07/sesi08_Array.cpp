44f0ece6d1039541eab24a5c37291e745e57836c
erickpaulus <erick_paulus@yahoo.com>
2024-10-07 12:16:11 +0200

    add code

---
+++ kode/sesi-07/sesi08_Array.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+main() { 
+    int bil[5] = {1,2,3,4,5};
+    for (int i : bil)
+    {
+        cout << i << endl;
+    }
+    getchar();
+
+}
+
---
+++ kode/sesi-07/sesi08_Array_passing.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+main() { 
+    int bil[5] = {1,2,3,4,5};
+    int angka[5];
+    for (int i : bil)
+    {
+        cout << i << endl;
+    }
+    
+    getchar();
+
+}
+
